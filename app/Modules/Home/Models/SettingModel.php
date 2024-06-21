8a71c5e615e277ad5e778967525a103239b611d1
dxvuong <36908851+dxvuong@users.noreply.github.com>
2024-06-21 16:42:56 +0700

    footer

---
+++ app/Modules/Home/Models/SettingModel.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Modules\Home\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SettingModel extends Model
+{
+    protected $table = 'settings';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+    ];
+
+}
